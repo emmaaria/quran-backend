6a013e00b6ef5289c3e020bdda57806b32fd208f
Emma <we@else.co.il>
2022-07-19 13:47:02 +0600

    New

--- database/migrations/2022_07_19_073346_create_suras_table.php
+++ database/migrations/2022_07_19_073346_create_suras_table.php
@@ -15,6 +15,9 @@ class CreateSurasTable extends Migration
     {
         Schema::create('suras', function (Blueprint $table) {
             $table->id();
+            $table->string('arabic_name');
+            $table->string('bangla_name');
+            $table->string('serial_no');
             $table->timestamps();
         });
     }
