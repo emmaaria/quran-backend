46d2512d7a5bbcae8ba422de81e63d99c2b471e9
Else Dev <admin@elkabetz.me>
2022-12-22 15:58:46 +0600

    New

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,7 +12,7 @@ class ApiController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth.custom:api', ['except' => ['login', 'getSuras', 'getChapters', 'getChapterBySura', 'searchChapters','getDuas', 'getDua']]);
+        $this->middleware('auth.custom:api', ['except' => ['login', 'getSuras', 'getChapters', 'getChapterBySura', 'searchChapters','getDuas', 'getDua', 'getLatestPosts']]);
     }
 
     protected function guard()
